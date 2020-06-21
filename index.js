bb6d1c871cf556154948056534968e00a2d5bcc6
Vincent Law <vincentlaw@MacBook-Pro.local>
2020-06-21 03:25:02 -0700

    postgresql

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 const { Pool } = require("pg");
 var pool;
 pool = new Pool({
-  connectionString: "postgres://postgres:root@localhost/users",
+  connectionString: process.env.DATABASE_URL,
 });
 
 var app = express();
